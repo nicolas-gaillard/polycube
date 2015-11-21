c175c28c0aac3a00f12140c665967ebb6fba456f
Hugo PIGEON <hugo.pigeon@etu.univ-nantes.fr>
2015-11-21 14:41:16 +0100

    Ajout de la signature de la fonction dans utils.py

---
+++ utils.py
@@ -0,0 +1,17 @@
+# utils.py
+# -*- coding: utf-8 -*-
+
+def colored54_to_perm48(colored54):
+    """Passage du format d'entrée avec les couleurs vers le format à base de permutations de (1..48).
+
+    :param colored54: les 54 facettes étiquetées par leur couleur (O, W, B, G, Y ou R)
+
+    :return: le même cube au format à base de permutations de (1..48)
+    """
+
+    pass
+
+
+if __name__=="__main__":
+    """Les tests des fonctions se font ici"""
+    pass
