9514db762feaf63d67f6c72d80d19fa66e6ab866
Hugo PIGEON <hugo.pigeon@etu.univ-nantes.fr>
2015-12-10 16:27:50 +0100

    Ajout de la signature des fonctions de résolution du cube

---
+++ resolution.py
@@ -0,0 +1,52 @@
+def premiere_face_couronne(cube):
+    """ Effectue la première face (la face blanche) ainsi que la première couronne
+
+    :param cube: objet de type cube (voir cube.py)
+    :return: une chaîne de caractères décrivant les mouvements à effectuer sur le cube
+    """
+    pass
+
+
+def deuxieme_couronne(cube):
+    """ Effectue la deuxième couronne
+
+    :param cube: objet de type cube (voir cube.py)
+    :return: une chaîne de caractères décrivant les mouvements à effectuer sur le cube
+    """
+    pass
+
+
+def croix(cube):
+    """ Effectue la croix
+
+    :param cube: objet de type cube (voir cube.py)
+    :return: une chaîne de caractères décrivant les mouvements à effectuer sur le cube
+    """
+    pass
+
+
+def correspondance(cube):
+    """ Effectue la correspondance des bords de la croix
+
+    :param cube: objet de type cube (voir cube.py)
+    :return: une chaîne de caractères décrivant les mouvements à effectuer sur le cube
+    """
+    pass
+
+
+def placement_coins(cube):
+    """ Effectue le placement des coins autour de la croix
+
+    :param cube: objet de type cube (voir cube.py)
+    :return: une chaîne de caractères décrivant les mouvements à effectuer sur le cube
+    """
+    pass
+
+
+def orientation_coins(cube):
+    """ Effectue l'orientation des coins autour de la croix
+
+    :param cube: objet de type cube (voir cube.py)
+    :return: une chaîne de caractères décrivant les mouvements à effectuer sur le cube
+    """
+    pass
