1bc98fb728b80182475f3952f4e0373904509f03
Hugo PIGEON <hugo.pigeon@etu.univ-nantes.fr>
2015-12-13 11:58:32 +0100

    Ecriture de la résolution de la croix, mais le cube n'est pas mis à jour

--- resolution.py
+++ resolution.py
@@ -1,3 +1,6 @@
+from cube import *
+from CubeDisplay import *
+
 def premiere_face_couronne(cube):
     """ Effectue la première face (la face blanche) ainsi que la première couronne
 
@@ -16,13 +19,59 @@ def deuxieme_couronne(cube):
     pass
 
 
+def croixTerminee(cube):
+    """ Teste l'état du cube pour déterminer si la croix est faite sur la face D (en jaune)
+
+    :param cube: objet de type cube
+    :return: True si la croix est faite, False sinon
+    """
+    faceD = cube.getFace(5)
+    jaunes = [42, 44, 45, 47] # facettes jaunes qui doivent former la croix
+    croixFaite = False
+    
+    if faceD[0][1] in jaunes and faceD[1][0] in jaunes and faceD[1][2] in jaunes and faceD[2][1] in jaunes:
+        croixFaite = True
+
+    return croixFaite
+        
+
 def croix(cube):
     """ Effectue la croix
 
     :param cube: objet de type cube (voir cube.py)
     :return: une chaîne de caractères décrivant les mouvements à effectuer sur le cube
     """
-    pass
+
+    # TODO : mettre à jour l'état du cube après chaque mouvement
+    mouvements = ""
+    jaunes = [42, 44, 45, 47] # facettes jaunes qui doivent former la croix
+    fini = croixTerminee(cube)
+    
+    while not fini:
+        faceD = cube.getFace(5)
+        if faceD[0][1] in jaunes and faceD[1][0] in jaunes:
+            mouvements += "R'D'B'DBR"
+
+            fini = True
+        elif faceD[1][0] in jaunes and faceD[2][1] in jaunes:
+            mouvements += "F'D'R'DRF"
+
+            fini = True
+        elif faceD[2][1] in jaunes and faceD[1][2] in jaunes:
+            mouvements += "L'D'F'DFL"
+
+            fini = True
+        else:
+            mouvements += "B'D'L'DLB"
+            
+            if faceD[0][1] in jaunes and faceD[1][2] in jaunes:
+                fini = True
+            else:
+                # boucle infinie car l'état du cube n'est pas changé pour l'instant (TODO)
+                fini = croixTerminee(cube)
+            
+    return mouvements
+        
 
 
 def correspondance(cube):
@@ -50,3 +99,10 @@ def orientation_coins(cube):
     :return: une chaîne de caractères décrivant les mouvements à effectuer sur le cube
     """
     pass
+
+
+if __name__ == "__main__":
+    #cube = cube("WWWWWWWWWGGGRRRBBBOOOGGGRRRBBBOOOYYBYBGYOBOYRRYOGYYGRY")
+    cube = cube("WWWWWWWWWGGGRRRBBBOOOGGGRRRBBBOOOGGBYYOYBRYYORRBYYYYOG")
+    drawCube(cube.cube_to_color54())
+    print(croix(cube))
