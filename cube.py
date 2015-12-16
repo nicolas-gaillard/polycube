09c4d924c2538c6d486d81549b028525c346c317
Alexandre DORMEVAL <alexandre.dormeval@etu.univ-nantes.fr>
2015-12-16 15:20:07 +0100

    changement de rotation face verte

--- cube.py
+++ cube.py
@@ -185,28 +185,31 @@ class cube :
         liste.append(self.lCube[2][2][0])
         liste.append(self.lCube[2][2][1])
         liste.append(self.lCube[2][2][2])
-        liste.append(self.lCube[1][2][2])
-        liste.append(self.lCube[1][2][1])
-        liste.append(self.lCube[1][2][0])
-        liste.append(self.lCube[4][2][2])
-        liste.append(self.lCube[4][2][1])
-        liste.append(self.lCube[4][2][0])
-        liste.append(self.lCube[3][2][2])
-        liste.append(self.lCube[3][2][1])
         liste.append(self.lCube[3][2][0])
+        liste.append(self.lCube[3][2][1])
+        liste.append(self.lCube[3][2][2])
+        liste.append(self.lCube[4][2][0])
+        liste.append(self.lCube[4][2][1])
+        liste.append(self.lCube[4][2][2])
+        liste.append(self.lCube[1][2][0])
+        liste.append(self.lCube[1][2][1])
+        liste.append(self.lCube[1][2][2])
 
         self.lCube[3][2][0]=liste[0]
         self.lCube[3][2][1]=liste[1]
         self.lCube[3][2][2]=liste[2]
+
         self.lCube[4][2][0]=liste[3]
         self.lCube[4][2][1]=liste[4]
         self.lCube[4][2][2]=liste[5]
-        self.lCube[1][2][0]=liste[6]
+
+        self.lCube[1][2][2]=liste[6]
         self.lCube[1][2][1]=liste[7]
-        self.lCube[1][2][2]=liste[8]
-        self.lCube[2][2][0]=liste[9]
+        self.lCube[1][2][0]=liste[8]
+
+        self.lCube[2][2][2]=liste[9]
         self.lCube[2][2][1]=liste[10]
-        self.lCube[2][2][2]=liste[11]
+        self.lCube[2][2][0]=liste[11]
 
     def echangeFace(self, face):
         print("debut echange face")
