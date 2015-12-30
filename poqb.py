0df84bc9e6363ed2e1114092d2ba01e82f9557cb
Hugo PIGEON <hugo.pigeon@etu.univ-nantes.fr>
2015-12-30 14:10:25 +0100

    Ajout de la fonction d'optimisation des manoeuvres

--- poqb.py
+++ poqb.py
@@ -1,6 +1,87 @@
 # poqb.py
 # -*- coding: utf-8 -*-
 
+def optimisation_manoeuvres(man):
+    """Réduit le nombre de manoeuvres à effectuer
+    :param man: la chaîne de caractères représentant les manoeuvres à effectuer
+    :return: une chaîne de caractères avec moins de manoeuvres (si possible)
+    """
+
+    mouv = ["U", "D", "L", "R", "F", "B"]
+    fini = False
+    manOpti = list(man)
+
+    while not fini:
+        fini = True
+        nouvManOpti = ""
+        l = len(manOpti)
+        i = 0
+        while i < l:
+            if manOpti[i] in mouv:
+                
+                # cas "UU", "RR", ...
+                if (i+1 < l and manOpti[i] == manOpti[i+1] and i+2 >= l) or (i+2 < l and manOpti[i] == manOpti[i+1] and manOpti[i+2] in mouv):
+                    fini = False
+                    nouvManOpti += manOpti[i] + "2"
+                    i += 1
+                        
+                # cas "U'U'", "R'R'", ...
+                elif i+3 < l and manOpti[i] == manOpti[i+2] and manOpti[i+1] == manOpti[i+3] == "'":
+                    fini = False
+                    nouvManOpti += manOpti[i] + "2"
+                    i += 3
+                    
+                # cas "U2U", "R2R", ...
+                elif (i+2 < l and manOpti[i] == manOpti[i+2] and manOpti[i+1] == "2" and i+3 >= l) or (i+3 < l and manOpti[i] == manOpti[i+2] and manOpti[i+1] == "2" and manOpti[i+3] in mouv):
+                    fini = False
+                    nouvManOpti += manOpti[i] + "'"
+                    i += 2
+                    
+                # cas "UU2", "RR2", ...
+                elif i+2 < l and manOpti[i] == manOpti[i+1] and manOpti[i+2] == "2":
+                    fini = False
+                    nouvManOpti += manOpti[i] + "'"
+                    i += 2
+                    
+                # cas "U2U'", "R2R'", ...
+                elif i+3 < l and manOpti[i] == manOpti[i+2] and manOpti[i+1] == "2" and manOpti[i+3] == "'":
+                    fini = False
+                    nouvManOpti += manOpti[i]
+                    i += 3
+                    
+                # cas "U'U2", "R'R2", ...
+                elif i+3 < l and manOpti[i] == manOpti[i+2] and manOpti[i+1] == "'" and manOpti[i+3] == "2":
+                    fini = False
+                    nouvManOpti += manOpti[i]
+                    i += 3
+                    
+                # cas "UU'", "RR'", ...
+                elif i+2 < l and manOpti[i] == manOpti[i+1] and manOpti[i+2] == "'":
+                    fini = False
+                    i += 2
+                    
+                # cas "U'U", "R'R", ...
+                elif (i+2 < l and manOpti[i] == manOpti[i+2] and manOpti[i+1] == "'" and i+3 >= l) or (i+3 < l and manOpti[i] == manOpti[i+2] and manOpti[i+1] == "'" and manOpti[i+3] in mouv):
+                    fini = False
+                    i += 2
+
+                # cas "U2U2", "R2R2", ...
+                elif i+3 < l and manOpti[i] == manOpti[i+2] and manOpti[i+1] == manOpti[i+3] == "2":
+                    fini = False
+                    i += 3
+
+                # pas de simplification possible
+                else:
+                    nouvManOpti += manOpti[i]
+            else:
+                nouvManOpti += manOpti[i]
+            i += 1
+                    
+        manOpti = str(nouvManOpti)
+
+    return manOpti
+
+
 def solve(cube_c54):
     """La fonction principale du projet qui résoud un Rubik's Cube.
 
