05d67bd7d7376adecfd5a3a05fcc5cc05eb795e6
Sheppard6 <kevin.briand@live.fr>
2016-01-06 14:28:16 +0100

    placements des coins

--- resolution.py
+++ resolution.py
@@ -64,7 +64,7 @@ def croix(cube):
     while not fini:
         faceD = cube.getFace(5)
         if faceD[0][1] in jaunes and faceD[1][0] in jaunes:
-            mouvements += "R"D"B"DBR"
+            mouvements += "R'D'B'DBR"
             cube.turnInv(3)
             cube.turnInv(5)
             cube.turnInv(4)
@@ -73,7 +73,7 @@ def croix(cube):
             cube.turn(3)
             fini = True
         elif faceD[1][0] in jaunes and faceD[2][1] in jaunes:
-            mouvements += "F"D"R"DRF"
+            mouvements += "F'D'R'DRF"
             cube.turnInv(2)
             cube.turnInv(5)
             cube.turnInv(3)
@@ -82,7 +82,7 @@ def croix(cube):
             cube.turn(2)
             fini = True
         elif faceD[2][1] in jaunes and faceD[1][2] in jaunes:
-            mouvements += "L"D"F"DFL"
+            mouvements += "L'D'F'DFL"
             cube.turnInv(1)
             cube.turnInv(5)
             cube.turnInv(2)
@@ -95,7 +95,7 @@ def croix(cube):
             if faceD[0][1] in jaunes and faceD[1][2] in jaunes:
                 fini = True
                 
-            mouvements += "B"D"L"DLB"
+            mouvements += "B'D'L'DLB"
             cube.turnInv(4)  
             cube.turnInv(5)  
             cube.turnInv(1)   
@@ -247,14 +247,195 @@ def rotH(cube,face):
 # end function
 
 
+def placementOK(cube):
+    """ On verifie si les coins sont bien placés
+
+    :param cube: objet de type cube (voir cube.py)
+    :return: booléen
+    """
+
+    #récupération des faces sauf up qui est finie
+    L=cube.getFace(1)
+    F=cube.getFace(2)
+    R=cube.getFace(3)
+    B=cube.getFace(4)
+    D=cube.getFace(5)
+
+    #recuperation des coins s'ils sont biens formés
+    LFD, RFD , LBD , RBD =[31,32,41] , [34,35,43] , [46,29,40] , [37,38,48]
+
+    #booléen de verification
+    coinsOK = False
+    bonCoin=[]
+
+    #test du bon placement des différents coins
+    if (L[2][0] in LBD) and (D[2][0] in LBD) and (B[2][2] in LBD): #ici le coin LBD
+        bonCoin.append('LBD')
+    if (L[2][2] in LFD) and (D[0][0] in LFD) and (F[2][0] in LFD): #ici le coin LFD
+        bonCoin.append('LFD')
+    if (R[2][0] in RFD) and (D[0][2] in RFD) and (F[2][2] in RFD): #ici le coin RFD
+        bonCoin.append('RFD')
+    if (R[2][2] in RBD) and (D[2][2] in RBD) and (B[2][0] in RBD): #ici le coin RBD
+        bonCoin.append('RBD')
+        
+    #si tous les coins sont biens placés on change le booléen
+                    
+    if len(bonCoin) == 4: coinsOK = True
+
+    return coinsOK,bonCoin
+
+#end function
 
 def placement_coins(cube):
-    """ Effectue le placement des coins autour de la croix
+    """ Effectue le placement des coins sur la face jaune, l'orientation sera faite après
 
     :param cube: objet de type cube (voir cube.py)
     :return: une chaîne de caractères décrivant les mouvements à effectuer sur le cube
     """
-    pass
+
+    LFD, RFD , LBD , RBD =[31,32,41] , [34,35,43] , [46,29,40] , [37,38,48]
+    mvm = ""
+
+    while not placementOK(cube)[0]:
+        #print(cube)
+        if placementOK(cube)[1] == []:
+            """Il n'y a aucun coin de bien placé donc on peut faire une des formules ci-dessous
+            peu importe laquelle qui permettra de placer un coin et d'enchainer"""
+            cube.turnInv(4)
+            cube.turn(2)
+            cube.turnInv(5)
+            cube.turn(4)
+            cube.turn(5)
+            cube.turnInv(2)
+            cube.turnInv(5)
+            cube.turnInv(4)
+            cube.turn(5)
+            cube.turn(4)
+            mvm+="B'FD'BDF'D'B'DB"
+
+        else:
+            if placementOK(cube)[1][0] == 'RFD':    #le coin RFD est bien placé
+
+                #si on doit tourner dans le sens horaire
+                if cube.getFace(1)[2][2] in RBD and cube.getFace(2)[2][0] in RBD and cube.getFace(5)[0][0] in RBD :
+                    cube.turnInv(4)
+                    cube.turn(2)
+                    cube.turn(5)
+                    cube.turnInv(2)
+                    cube.turnInv(5)
+                    cube.turn(4)
+                    cube.turn(5)
+                    cube.turn(2)
+                    cube.turnInv(5)
+                    cube.turnInv(2)
+                    mvm+="B'FDF'D'BDFD'F'"
+                    print(cube)
+
+                # sens antihoraire
+                else:
+                    cube.turnInv(3)
+                    cube.turn(1)
+                    cube.turnInv(5)
+                    cube.turn(3)
+                    cube.turn(5)
+                    cube.turnInv(1)
+                    cube.turnInv(5)
+                    cube.turnInv(3)
+                    cube.turn(5)
+                    cube.turn(3)
+                    mvm += "R'LD'RDL'D'RDR"
+                    
+                
+            elif placementOK(cube)[1][0] == 'LFD':    #le coin LFD est bien placé
+                #sens horaire
+                if cube.getFace(1)[2][0] in RFD and cube.getFace(4)[2][2] in RFD and cube.getFace(5)[2][0] in RFD :
+                    cube.turnInv(3)
+                    cube.turn(1)
+                    cube.turn(5)
+                    cube.turnInv(1)
+                    cube.turnInv(5)
+                    cube.turn(3)
+                    cube.turn(5)
+                    cube.turn(1)
+                    cube.turnInv(5)
+                    cube.turnInv(1)
+                    mvm+="R'LDL'D'RDLD'L'"
+
+                #sens antihoraire
+                else:
+                    cube.turnInv(2)
+                    cube.turn(4)
+                    cube.turnInv(5)
+                    cube.turn(2)
+                    cube.turn(5)
+                    cube.turnInv(4)
+                    cube.turnInv(5)
+                    cube.turnInv(2)
+                    cube.turn(5)
+                    cube.turn(2)
+                    mvm+="F'BD'FDB'D'F'DF"
+
+            elif placementOK(cube)[1][0] == 'LBD':    #le coin LBD est bien placé
+                #sens horaire
+                if cube.getFace(3)[2][2] in LFD and cube.getFace(4)[2][0] in LFD and cube.getFace(5)[2][2] in LFD :
+                    cube.turnInv(2)
+                    cube.turn(4)
+                    cube.turn(5)
+                    cube.turnInv(4)
+                    cube.turnInv(5)
+                    cube.turn(2)
+                    cube.turn(5)
+                    cube.turn(4)
+                    cube.turnInv(5)
+                    cube.turnInv(4)
+                    mvm+="F'BDB'D'FDBD'B'"
+
+                #sens antihoraire
+                else:
+                    cube.turnInv(1)
+                    cube.turn(3)
+                    cube.turnInv(5)
+                    cube.turn(1)
+                    cube.turn(5)
+                    cube.turnInv(3)
+                    cube.turnInv(5)
+                    cube.turnInv(1)
+                    cube.turn(5)
+                    cube.turn(1)
+                    mvm+="L'RD'LDR'D'L'DL"
+
+            elif placementOK(cube)[1][0] == 'RBD':  #le coin RBD est bien placé
+                #sens horaire
+                if cube.getFace(2)[2][2] in LBD and cube.getFace(3)[2][0] in LBD and cube.getFace(5)[0][2] in LBD :
+                    cube.turnInv(1)
+                    cube.turn(3)
+                    cube.turn(5)
+                    cube.turnInv(3)
+                    cube.turnInv(5)
+                    cube.turn(1)
+                    cube.turn(5)
+                    cube.turn(3)
+                    cube.turnInv(5)
+                    cube.turnInv(3)
+                    mvm+="L'RDR'D'LDRD'R'"  
+
+                #sens antihoraire
+                else:          
+                    cube.turnInv(4)
+                    cube.turn(2)
+                    cube.turnInv(5)
+                    cube.turn(4)
+                    cube.turn(5)
+                    cube.turnInv(2)
+                    cube.turnInv(5)
+                    cube.turnInv(4)
+                    cube.turn(5)
+                    cube.turn(4)
+                    mvm+="B'FD'BDF'D'B'DB"
+
+    print(cube)
+    return mvm       
+#end function
 
 
 def orientation_coins(cube):
