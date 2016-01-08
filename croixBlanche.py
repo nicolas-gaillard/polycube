1e6050b42efd012d020f583b083ab5fb264a5ca2
Valentin Bouchevreau <valentin.bouchevreau@etu.univ-nantes.fr>
2016-01-08 11:49:03 +0100

    Architecture fichier croix blanche

---
+++ croixBlanche.py
@@ -0,0 +1,10 @@
+from cube import *
+from CubeDisplay import *
+
+def croixBlanche(cube):
+
+if __name__ == "__main__" :
+
+	cube = cube("YWBWWWBWYBGOWRGOBWROOYGYRRGYBOGOOYRGORGROWRYBWGYBYBRBG")
+
+	drawCube(cube.cube_to_color54())
