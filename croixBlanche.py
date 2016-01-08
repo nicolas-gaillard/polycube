8469c61a4fa26da5989d47a07e2559e153b406a9
Valentin Bouchevreau <valentin.bouchevreau@etu.univ-nantes.fr>
2016-01-08 11:55:37 +0100

    ajout de la fonction de verification

--- croixBlanche.py
+++ croixBlanche.py
@@ -3,6 +3,15 @@ from CubeDisplay import *
 
 def croixBlanche(cube):
 
+	mouvement=""
+
+def croixBlancheDone(cube):
+	""" Cette fonction nous permet de savoir si la croix blanche est terminé ou non
+	Pour cela on regarde si au position de la croix blanche il y a bien les 4 facettes
+	blanche et celle qui ne bouge pas au centre"""
+	croixDone = False
+	if cube.lCube[0][0][1] == "W" and cube.lCube[0][1][0] == "W" and cube.lCube[0][1][2] == "W" and cube.lCube[0][2][1] == "W"
+
 if __name__ == "__main__" :
 
 	cube = cube("YWBWWWBWYBGOWRGOBWROOYGYRRGYBOGOOYRGORGROWRYBWGYBYBRBG")
