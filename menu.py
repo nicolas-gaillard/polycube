dea5b3ec5c7863c68858a8a7ba48281a4dcb4734
Nicolas Gaillard <nicolas.gaillard@etu.univ-nantes.fr>
2016-01-13 11:06:11 +0100

    Ajout fonctionnalités menu

--- menu.py
+++ menu.py
@@ -25,7 +25,7 @@ def affichageMenu():
     print("|                                       |")
     print("| - 1 - Résolution d'un cube aléatoire  |")
     print("| - 2 - Résolution d'un cube précis     |")
-    print("| - 3 - Afficher un cube                |")
+    print("| - 3 - Afficher un cube précis         |")
     print("| - 4 - Résolution via alg.cubing.net   |")
     print("| - 5 - README      				       |")
     print("| - 6 - PERFORMANCES      			   |")
@@ -33,8 +33,8 @@ def affichageMenu():
     print("|                                       |")
     print("\\-------                        --------/")
     print("")
-    choix = input("Que faire ?")
-    gestionMenu(int(choix))
+    choix = input("Que faire ? ")
+    choixMenu(int(choix))
 
 def menu():
 	clear()
@@ -43,12 +43,52 @@ def menu():
 
 def choixMenu(choix):
     if choix == 1 :
+        clear()
+        color54 = generator()
+        cube = cube(color54)
+
+        print("Affichage du cube à résoudre")
+        print(cube)
+        print("Chaîne de mouvements : "+solve(generator()))
+        os.system("pause")
+
+        pageAccueil()
+        affichageMenu()
+
     elif choix == 2 :
+        color54 = input("Entrez une chaine de 54 couleurs correcte : ")
+        clear()
+        cube = cube(color54)
+        print(cube)
+
+        print("Chaîne de mouvements : "+solve(color54))
+        os.system("pause")
+        pageAccueil()
+        affichageMenu()
+
     elif choix == 3 :
+        color54 = input("Entrez une chaîne de 54 couleurs valide : ")
+        cube = cube(color54)
+        print(cube)
+
+        pageAccueil()
+        affichageMenu()
+
     elif choix == 4 :
         a,b = scramble()
         c=solve(b).replace("'","-")
         webbrowser.open("https://alg.cubing.net/?setup="+a+"&alg="+c)
+
+    elif choix == 5 :
+        readme = open("README.md", "r")
+        print(readme.read())
+
+        os.system("pause")
+        readme.close()
+
+        pageAccueil()
+        affichageMenu()
+
     elif choix == 0 :
         clear()
         print("Merci d'avoir utilisé Polycube")
